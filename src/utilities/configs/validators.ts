bb1f7e9ff87f240cf8b0b6f00b5803d7e0f62a53
Viet Ngoc <ngocjohn@me.com>
2025-05-06 23:03:28 +0200

    refactor: enhance sidebar dialog with config file validation and error rendering

--- src/utilities/configs/validators.ts
+++ src/utilities/configs/validators.ts
@@ -31,36 +31,54 @@ export const validateConfig = (config: SidebarConfig, hidden?: string[]): Sideba
   return newConfig;
 };
 
-export const isItemsValid = (config: SidebarConfig, hass: HaExtened['hass']): boolean => {
+export const isItemsValid = (
+  config: SidebarConfig,
+  hass: HaExtened['hass'],
+  log: boolean = false
+):
+  | boolean
+  | {
+      configValid: boolean;
+      duplikatedItems: string[];
+      invalidItems: string[];
+      noTitleItems: string[];
+    } => {
   const allItems = [
     ...Object.values(config.custom_groups || {}).flat(),
     ...(config.bottom_items || []),
     ...(config.hidden_items || []),
   ];
 
-  if (allItems.length === 0) return false;
-
+  if (allItems.length === 0) {
+    return log ? { configValid: false, duplikatedItems: [], invalidItems: [], noTitleItems: [] } : false;
+  }
+  const customGroups = Object.values(config.custom_groups || {}).flat();
+  const duplikatedItems = customGroups.filter((item, index) => customGroups.indexOf(item) !== index) || [];
   const haPanelKeys = Object.keys(hass.panels);
   const invalidItems = allItems.filter((item) => !haPanelKeys.includes(item));
   const noTitleItems = allItems.filter((item) => hass.panels[item] && !hass.panels[item].title);
 
-  if (invalidItems.length > 0) {
-    // console.warn(`${CONFIG_NAME}: Config is not valid. Diff items: ${invalidItems.join(', ')}`);
-    LOGGER.warn(`${CONFIG_NAME}: Config is not valid. Diff items: ${invalidItems.join(', ')}`);
+  const configValid = duplikatedItems.length === 0 && invalidItems.length === 0 && noTitleItems.length === 0;
 
+  if (duplikatedItems.length > 0) {
+    LOGGER.warn(`${CONFIG_NAME}: Config is not valid. Duplicated items: ${duplikatedItems.join(', ')}`);
+    console.table(duplikatedItems);
+  }
+
+  if (invalidItems.length > 0) {
+    LOGGER.warn(`${CONFIG_NAME}: Config is not valid. Diff items: ${invalidItems.join(', ')}`);
     console.table(invalidItems);
   }
 
   if (noTitleItems.length > 0) {
-    // console.warn(`${CONFIG_NAME}: Items not showing in sidebar: ${noTitleItems.join(', ')}`);
     LOGGER.warn(`${CONFIG_NAME}: Items not showing in sidebar: ${noTitleItems.join(', ')}`);
-
     console.table(noTitleItems);
   }
 
-  const configValid = invalidItems.length === 0 && noTitleItems.length === 0;
+  if (log) {
+    return { configValid, duplikatedItems, invalidItems, noTitleItems };
+  }
 
-  // console.log('configValid', configValid);
   return configValid;
 };
 
