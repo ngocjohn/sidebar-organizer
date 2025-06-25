80d2c47aa14e8c19e51d848086fcd6d94fa0449a
Viet Ngoc <ngocjohn@me.com>
2025-06-25 19:25:18 +0200

    refactor: remove unused import from loader utility

--- src/utilities/loader.ts
+++ src/utilities/loader.ts
@@ -1,5 +1,3 @@
-import { HomeAssistant } from 'custom-card-helpers';
-
 // Hack to load ha-components needed for editor
 export const loadHaComponents = () => {
   if (!customElements.get('ha-form')) {
